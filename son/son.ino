1ad42e9216a8ea67f3b698ba0e619a9c437ee4c8
kenza02 <100873607+kenza02@users.noreply.github.com>
2023-04-05 01:21:00 +0000

    Add files via upload

---
+++ son/son.ino
@@ -0,0 +1,19 @@
+// Arduino code to read data from HC-SR04
+
+#include <NewPing.h>
+
+#define TRIGGER_PIN 2
+#define ECHO_PIN 3
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN);
+int distance;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(2000); // Add delay for stability
+  distance = sonar.ping_cm(); // Get distance in cm
+  Serial.println(distance); // Send distance data to Raspberry Pi via Serial
+}
