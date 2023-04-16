f4314f48d33aec7bc18d53c5fb00e4db66148586
kenza02 <100873607+kenza02@users.noreply.github.com>
2023-04-16 02:56:48 +0000

    Add files via upload
    This code allow us to see see if a place in our parking is occupied or not, via the two leds and the ultrasound sensor

---
+++ son.ino
@@ -0,0 +1,53 @@
+const int trig = 12;
+const int echo = 13;
+
+const int LED1 = 8;
+const int LED2 = 7;
+
+
+int duration = 0;
+int distance = 0;
+
+void setup() 
+{
+  pinMode(trig , OUTPUT);
+  pinMode(echo , INPUT);
+  
+  pinMode(LED1 , OUTPUT);
+  pinMode(LED2 , OUTPUT);
+  
+  
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  digitalWrite(trig , HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trig , LOW);
+
+
+  duration = pulseIn(echo , HIGH);
+  distance = (duration/2) / 28.5 ;
+  Serial.println(distance);
+  
+
+  if ( distance <= 7 )
+  {
+    digitalWrite(LED1, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED1, LOW);
+  }
+  if ( distance > 8 )
+  {
+    digitalWrite(LED2, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED2, LOW);
+  }
+}
+
